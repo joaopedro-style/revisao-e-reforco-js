e97639912498cd93a541c5859f33743216dbe5e5
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-06 09:10:11 -0300

    Aplica função nomeada

---
+++ 03-funcoes.js
@@ -0,0 +1,11 @@
+// 03-funcoes.js
+'use strict';
+
+exemplo1(); // chamada da função
+
+/* Sintaxe tradicional, função nomeada a ordem de declaração da função neste caso NÂO IMPORTA */
+function exemplo1(){
+    console.log("Função nomeada");
+}
+
+exemplo1(); // chamada da função
