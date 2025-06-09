0b4aac75c2140efa48b0cef91242225aa4cad2cd
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-09 09:38:32 -0300

    Adiciona separador aos módulos

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -1,8 +1,13 @@
 'use strict';
 // 05-usando-modulos-esm.js
 
+// Importação de recurso padrão (default)
+import alunos from './modulos-esm/alunos.js';
+
 // Importação de mútiplos recursos/módulos usando ESM
-import { saudacao, clientes } from './modulos-esm/exemplos.js';
+import { saudacao, clientes, separador } from './modulos-esm/exemplos.js';
 
 saudacao("Fulano de Tal");
 console.log(clientes);
+
+separador();
--- modulos-esm/exemplos.js
+++ modulos-esm/exemplos.js
@@ -1,10 +1,14 @@
 'use strict';
 
 /* Módulos ESM: EcmaScript Modules (sistema atual, moderno) */
-function saudacao(nome){
-    console.log("Olá "+nome+"!");
+function saudacao(nome) {
+    console.log("Olá " + nome + "!");
 }
 
 const clientes = ["João", "Pedro", "Gabriel"];
 
-export {saudacao, clientes}
+function separador() {
+    console.log("\n**********************************\n");
+};
+
+export { saudacao, clientes, separador }
