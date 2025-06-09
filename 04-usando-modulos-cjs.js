cebb246e5d95c2aecb6c96e5886fb8318640f356
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-09 09:05:56 -0300

    cria arquivo 04

---
+++ 04-usando-modulos-cjs.js
@@ -0,0 +1,11 @@
+'use strict';
+// 04-usando-modulos-cjs.js
+
+// 'Importando' os módulos usando CJS require
+const modulosExemplos = require('./modulos-cjs/exemplos.js');
+
+// Chamando a função existente no módulo
+modulosExemplos.minhaFuncao();
+
+// Acessando o array através do módulo
+console.log(modulosExemplos.clientes);
