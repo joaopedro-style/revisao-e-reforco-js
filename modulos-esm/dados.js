a61f14b52f4c18ae8d9b7fcc96b73bf0298eab22
joaopedro-style <joaopedro18231@hotmail.com>
2025-06-09 10:02:27 -0300

    Adiciona import com alias/apelido e acessa recursos do módulo dados.js

---
+++ modulos-esm/dados.js
@@ -0,0 +1,18 @@
+'use strict';
+// dados.js
+
+const pessoa = {
+    nome: 'João',
+    idade: 28,
+    cidade: 'São Paulo'
+};
+
+const livro = {
+    titulo: 'O Senhor dos Anéis',
+    volume: 'As Duas Torres',
+    ano: 1954
+};
+
+const alunos = ['Pica Pau', 'Zé colméia', 'Naruto', 'Luffy'];
+
+export { pessoa, livro, alunos }
